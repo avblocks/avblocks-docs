16328feaf1b655e5729722684e6a1f3cb2847a8c
vkantchev <vkantchev@users.noreply.github.com>
2024-05-07 19:34:23 -0700

    Auto push on 2024-05-07 19:34:22

--- net/latest/search_config.php
+++ net/latest/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<div id=\"side-nav\" class=\"ui-resizable side-nav-resizable\">\n  <div id=\"nav-tree\">\n    <div id=\"nav-tree-contents\">\n      <div id=\"nav-sync\" class=\"sync\"></div>\n    </div>\n  </div>\n  <div id=\"splitbar\" style=\"-moz-user-select:none;\" \n       class=\"ui-resizable-handle\">\n  </div>\n</div>\n<script type=\"text/javascript\">\n/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&amp;dn=expat.txt MIT */\n$(document).ready(function(){initNavTree('search.html',''); initResizable(); });\n/* @license-end */\n</script>\n<div id=\"doc-content\">\n",
-  'logo' => "Generated on Tue May 7 2024 18:53:14 for AVBlocks for .NET by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.7 ",
+  'logo' => "Generated on Tue May 7 2024 19:34:19 for AVBlocks for .NET by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.7 ",
 );
 
 ?>
